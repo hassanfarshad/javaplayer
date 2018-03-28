9ea3d13953fe5087c6b57e66b6d522f9c07d50e9
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-28 16:09:37 +0530

    Create ep1.java

---
+++ ep1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+class ep1
+{
+  public static void main(String args[])
+  {
+  Scanner d=new Scanner(System.in);
+  String a=d.next();
+  String b = new StringBuffer(a).reverse().toString();
+  System.out.println(b);
+  }
+ }
