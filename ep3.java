77794bd5024ac5f61bdbecb1e10f38d76e225992
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-28 16:17:02 +0530

    Create ep3.java

---
+++ ep3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+class ep3
+{
+  public static void main(String args[])
+  {
+  Scanner d=new Scanner(System.in);
+  String c=d.next();
+  String e = new StringBuffer(c).reverse().toString();
+  System.out.println(e);
+  }
+ }
