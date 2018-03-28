2d722f3525d4b24657775eb49457e592161c9777
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-28 16:29:23 +0530

    Create ep4.java

---
+++ ep4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+class ep4
+{
+  public static void main(String args[])
+  {
+          Scanner m=new Scanner(System.in);
+          String p=m.next();
+          System.out.println(p+".");
+  }
+ }
